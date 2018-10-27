c97a424554c757c24643ce93133f69bac6f6fe3e
Daniil Sudzhaev <sudzhaev.da@gmail.com>
2018-10-28 00:15:52 +0300

    fixed p tag in message

--- server.py
+++ server.py
@@ -2,6 +2,10 @@ from bottle import request, Bottle
 from telegram import Bot
 
 
+def prepare_msg(message):
+    return '\n'.join(''.join(message.split('<p>')).split('</p>'))
+
+
 class Server(Bottle):
     def __init__(self, bot: Bot):
         super(Server, self).__init__()
@@ -10,7 +14,7 @@ class Server(Bottle):
 
     def send_post(self):
         message = request.json
-        msg_text = message['message']
+        msg_text = prepare_msg(message['message'])
         receivers = message['users']
         for user in receivers:
             self.bot.send_message(chat_id=user, text=msg_text, parse_mode='HTML')
